7f9eecc60e2f7bc1f9501d78d85299cb161e55de
Luigi <luigibyte@gmail.com>
2024-02-24 01:03:08 -0300

    basic CRUd for users

---
+++ src/lib/db.ts
@@ -0,0 +1,10 @@
+import mongoose, { Mongoose } from "mongoose";
+
+export const connect = async (): Promise<void> => {
+  try {
+    await mongoose.connect(process.env.MONGOOSE!);
+    console.log('db connected', process.env.MONGOOSE!);
+  } catch (error) {
+    console.log(error);
+  }
+}
