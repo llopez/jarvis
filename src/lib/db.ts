64fdad06c2c4171257b7532a50a9acad9371056c
Luigi <luigibyte@gmail.com>
2024-02-28 00:07:56 -0300

    db logs

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
+mongoose.connection.on("connected", () => console.log("connected"));
+mongoose.connection.on("open", () => console.log("open"));
+mongoose.connection.on("disconnected", () => console.log("disconnected"));
+mongoose.connection.on("reconnected", () => console.log("reconnected"));
+mongoose.connection.on("disconnecting", () => console.log("disconnecting"));
+mongoose.connection.on("close", () => console.log("close"));
+
 export const connect = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGOOSE!);
-    console.log('db connected', process.env.MONGOOSE!);
+    await mongoose.connect(process.env.MONGOOSE!, { maxPoolSize: 10 });
   } catch (error) {
     console.log(error);
   }
-}
+};
