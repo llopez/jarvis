f6b05037303f4d30fa9ce7eed044098773cb6722
Luigi <luigibyte@gmail.com>
2024-02-28 00:31:46 -0300

    improve home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 const navigation = [
   { name: "Product", href: "#" },
   { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
   { name: "Company", href: "#" },
 ];
 
@@ -54,23 +53,12 @@ export default function Home() {
 
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{" "}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
-            </div>
-          </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Data to enrich your online business
+              Google Home Integration
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-              fugiat aliqua.
+              This project is an example of a google home Integration cloud-to-cloud
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
