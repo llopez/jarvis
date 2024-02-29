897fbaf9c8a96860c0d5ae5d7172432d6160d451
Luigi <luigibyte@gmail.com>
2024-02-29 01:27:07 -0300

    authorize form design

--- src/app/auth/authorize/page.tsx
+++ src/app/auth/authorize/page.tsx
@@ -9,5 +9,28 @@ export default async function Authorize() {
     redirect("/auth");
   });
 
-  return <h1>Authorize Google</h1>;
-};
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Authorize Google
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm flex gap-2">
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Cancel
+        </button>
+        <button
+          type="submit"
+          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Authorize
+        </button>
+      </div>
+    </div>
+  );
+}
