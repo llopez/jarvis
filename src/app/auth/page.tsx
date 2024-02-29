3fbda7f612ae56a306e55eab7e2e9cc62e6aceae
Luigi <luigibyte@gmail.com>
2024-02-29 15:08:25 -0300

    auth flow almost completed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,5 +1,8 @@
 import { checkAuth } from "@/app/actions";
 import { redirect } from "next/navigation";
+import { User } from "@/models/User";
+import crypto from "crypto";
+import { revalidatePath } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
@@ -8,7 +11,7 @@ export default async function Authorize({
 }: {
   searchParams: { [key: string]: string };
 }) {
-  await checkAuth(async (auth) => {
+  const user = await checkAuth(async (auth) => {
     if (auth) {
       return auth;
     }
@@ -18,6 +21,32 @@ export default async function Authorize({
     redirect("/auth/signin?".concat(carryOnParams));
   });
 
+  const allow = async () => {
+    "use server";
+
+    const token = crypto.randomBytes(64).toString("hex");
+
+    if (!user) return;
+
+    await User.updateOne({ _id: user._id }, { token });
+
+    const { redirect_uri, state } = searchParams;
+
+    const queryParams = new URLSearchParams({ state, token }).toString();
+
+    redirect(redirect_uri.concat("?").concat(queryParams));
+  };
+
+  const deny = async () => {
+    "use server";
+
+    if (!user) return;
+
+    await User.updateOne({ _id: user._id }, { token: null });
+
+    revalidatePath("/auth");
+  };
+
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -26,19 +55,25 @@ export default async function Authorize({
         </h2>
       </div>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm flex gap-2">
-        <button
-          type="submit"
-          className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Authorize
-        </button>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm flex gap-2 justify-center">
+        <form action={deny}>
+          <button
+            disabled={!user?.token}
+            className="flex w-full justify-center rounded-md bg-red-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Revoke
+          </button>
+        </form>
+
+        <form action={allow}>
+          <button
+            disabled={!!user?.token}
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Grant
+          </button>
+        </form>
       </div>
     </div>
   );
--- src/models/User.ts
+++ src/models/User.ts
@@ -3,12 +3,14 @@ import { Schema, model, models } from "mongoose";
 export type UserType = {
   email: string;
   hash: string;
+  token: string;
   _id: string;
-}
+};
 
 const schema = new Schema({
   email: { type: String, required: true, unique: true },
   hash: String,
+  token: { type: String, unique: true },
 });
 
 export const User = models.User || model("User", schema);
