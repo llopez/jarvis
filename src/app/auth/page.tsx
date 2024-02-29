f13ef75a1e69a7ca9245a055817832c6f89f6f21
Luigi <luigibyte@gmail.com>
2024-02-29 10:43:14 -0300

    adding UI logging to understand how it works

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,15 @@
 import Link from "next/link";
 import { Form } from "./Form";
 
-export default async function SignIn() {
+type ParamsType = {
+  [key: string]: string;
+};
+
+export default async function SignIn({
+  searchParams,
+}: {
+  searchParams: ParamsType;
+}) {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -11,15 +19,27 @@ export default async function SignIn() {
           </h2>
         </div>
 
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          {Object.keys(searchParams).map((key) => (
+            <li key={key}>
+              {key}: {searchParams[key]}
+            </li>
+          ))}
+        </div>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <Form />
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <Link href='/signup' className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Sign Up</Link>
+            Not a member?{" "}
+            <Link
+              href="/signup"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Sign Up
+            </Link>
           </p>
         </div>
       </div>
     </>
-  )
+  );
 }
