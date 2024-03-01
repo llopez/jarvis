dd37028b407f438ae7779af09cb4ca23377ccffd
Luigi <luigibyte@gmail.com>
2024-03-01 15:54:33 -0300

    validate cliend_id and client_secret

--- src/app/token/route.ts
+++ src/app/token/route.ts
@@ -4,26 +4,23 @@ import crypto from "crypto";
 
 export const POST = async (req: NextRequest) => {
   const data = await req.formData();
-
   const client_id = data.get("client_id")?.toString();
   const client_secret = data.get("client_secret")?.toString();
   const grant_type = data.get("grant_type")?.toString();
+  // const redirect_uri = data.get("redirect_uri")?.toString();
 
-  console.log({
-    client_id,
-    client_secret,
-    grant_type,
-  });
+  if (client_id !== process.env.GOOGLE_CLIENT_ID) {
+    return Response.json({ error: "invalid_grant" }, { status: 400 });
+  }
+
+  if (client_secret !== process.env.GOOGLE_CLIENT_SECRET) {
+    return Response.json({ error: "invalid_grant" }, { status: 400 });
+  }
 
   const accessToken = crypto.randomBytes(32).toString("hex");
 
-  console.log("grant_type", grant_type);
-
   if (grant_type === "authorization_code") {
-    console.log("ADJSADJSDHJSD");
-
     const code = data.get("code")?.toString();
-    const redirect_uri = data.get("redirect_uri")?.toString();
 
     const user = await User.findOne({ authorizationCode: code });
 
@@ -31,14 +28,10 @@ export const POST = async (req: NextRequest) => {
       return Response.json({ error: "invalid_grant" }, { status: 400 });
     }
 
-    // TODO: request validation
-
     const refreshToken = crypto.randomBytes(32).toString("hex");
     const expiresIn = 60 * 10; // 10 minutes
 
     await User.updateOne({ _id: user._id }, { accessToken, refreshToken });
-
-    // TODO: save expiresIn into User
 
     return Response.json({
       token_type: "Bearer",
@@ -47,8 +40,6 @@ export const POST = async (req: NextRequest) => {
       expires_in: expiresIn,
     });
   } else {
-    console.log("REFRESH");
-
     const refresh_token = data.get("refresh_token")?.toString();
 
     const user = await User.findOne({ refreshToken: refresh_token });
@@ -57,12 +48,8 @@ export const POST = async (req: NextRequest) => {
       return Response.json({ error: "invalid_grant" }, { status: 400 });
     }
 
-    // TODO: request validation
-
     const expiresIn = 60 * 60 * 24; // 1 day
     await User.updateOne({ _id: user._id }, { accessToken });
-
-    // TODO: save expiresIn into User
 
     return Response.json({
       token_type: "Bearer",
